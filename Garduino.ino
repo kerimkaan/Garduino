4bfa66e757ec42e7bb2f73b96f8b594636116ba6
Kerim Kaan <euphemiatr@gmail.com>
2017-07-05 13:29:26 +0300

    Added soil humidity detecting & changed water level detecting

--- Garduino.ino
+++ Garduino.ino
@@ -2,13 +2,11 @@
  * Garduino | 0.1
  * Plant Monitoring & Management System on Arduino
  *
- * Water level detecting: Sensor(A0), led1(7), integers(watersensor,WaterSensorValue)
  * DHT temp & humidity detecting: Sensor(Digital 2), integers(h,t,hic)
  * Servo motor controlling: Motor(Digital 9), integers(motor,pos)
- * IR Controlling: Receiver(Digital 13), integers(RECV_PIN)
- * Distance detecting: Sensor(Digital 3 & 2), VCC to Arduino 5v GND to Arduino GND
- *
- * Created by Kerim Kaan (kerimkaan.com)
+ * IR Controlling (Optional,Not Include): Receiver(Digital 13), integers(RECV_PIN)
+ * Water level (Distance) detecting: Sensor(Digital 3 & 2), VCC to Arduino 5v GND to Arduino GND
+ * Soil humidity detecting: Sensor(A0), VCC to 5V
  *
  *
  * Before use please read README.md file.
@@ -31,17 +29,18 @@ int pos = 0;
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-// Distance sensor (HC-SR04) Pins
-#define trigPin 3
-#define echoPin 2
+// Distance sensor (HC-SR04) for water level detecting (HC-SR04) Pins
+#define trigPin 3 // Digital 3
+#define echoPin 2 // Digital 2
 
 // LEDs
-int led1 = 7; // LED1(Mavi) 7. pine bağlı
-int led2 = 12; // LED2(Sarı) 12. pine bağlı
-int led3 = 11;  // LED3(Kırmızı) 11. pine bağlı
+int led1 = 7; // LED1(Mavi, Blue) 7. pine bağlı
+int led2 = 12; // LED2(Sarı, Yellow) 12. pine bağlı
+int led3 = 11;  // LED3(Kırmızı, Red) 11. pine bağlı
 
-// Water level sensor
-int watersensor = 0; // Water sensor integer, Arduino A0
+// Soil humidity sensor
+int soilsensor = 0; // Soil humidity sensor integer, Arduino A0
+int nem; // Humidity integer
 
 
 void setup() {
@@ -55,8 +54,11 @@ void setup() {
 }
 
 void loop() {
- // Temperature & humidity detecting, monitoring
- int WaterSensorValue = analogRead(watersensor);
+ // Soil humidity detecting
+  nem= analogRead(soilsensor);
+  nem= map(nem,0,1023,0,100);
+
+  // Enviroment temperature & humidity detecting, monitoring
   float h = dht.readHumidity(); // Read humidity (%) - Nem oranı
   float t = dht.readTemperature(); // Read temperature as Celsius - Celsius cinsinden sıcaklık değeri
   float hic = dht.computeHeatIndex(t, h, false); // Compute heat index in Celsius - Isı indeksi (Hissedilen sıcaklık)
@@ -70,8 +72,10 @@ void loop() {
   Serial.print("Isi Indeksi(Hissedilen Sicaklik) / Heat Index: ");
   Serial.print(hic);
   Serial.println(" *C ");
+  Serial.print("Toprak Nemi / Soil Humidity: ");
+  Serial.println(nem);
 
-  // Distance detecting & monitoring
+  // Water level detecting & monitoring
   long duration, distance;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -80,18 +84,20 @@ void loop() {
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
   distance = (duration/2) / 29.1;
+  Serial.print("Su seviyesi / Water level: ");
   Serial.print(distance);
   Serial.println(" cm");
 
-  if (distance < 10){
-    Serial.println("Su seviyesi çok düşük / Water level is too low")
-    digitalWrite(led1, HIGH);
-    digitalWrite(led2, HIGH);
-    digitalWrite(led3, HIGH);
+  if (distance < 5){
+    Serial.println("Su seviyesi çok düşük / Water level is too low");
+    Serial.print("Su seviyesi çok düşük / Water level is too low");
+    digitalWrite(led1,HIGH);
+    digitalWrite(led2,HIGH);
+    digitalWrite(led3,HIGH);
     Serial.println("-------------------------------------------------------------------");
   }
   else{
-    if (WaterSensorValue >= 500){
+    if (nem  >= 0 && nem  < 20){
      Serial.println("Toprak nem seviyesi / Soil humidity level: TOO HIGH");
      digitalWrite(led3,HIGH);
      delay(250);
@@ -101,25 +107,25 @@ void loop() {
      delay(250);
      digitalWrite(led3,LOW);
     }
-    else if (WaterSensorValue >= 400 && WaterSensorValue < 500)
+    else if (nem  >= 20 && nem  < 50)
     {
      Serial.println("Toprak nem seviyesi / Soil humidity level: HIGH");
      digitalWrite(led2,HIGH);
      delay(500);
      digitalWrite(led2,LOW);
     }
-    else if (WaterSensorValue >= 250 && WaterSensorValue < 400)
+    else if (nem  >= 50 && nem  < 75)
     {
      Serial.println("Toprak nem seviyesi / Soil humidity level: NORMAL");
      digitalWrite(led1,LOW);
      digitalWrite(led2,LOW);
      digitalWrite(led3,LOW);
     }
-    else if (WaterSensorValue >= 0 && WaterSensorValue < 250)
+    else if (nem  >= 75 && nem  < 100)
     {
      digitalWrite(led1,HIGH);
      Serial.println("Toprak nem seviyesi / Soil humidity level: LOW");
-
+     Serial.println("Sulama yapiliyor... / Watering...");
      for (pos = 0; pos <= 90; pos += 5) {
        // in steps of 5 degrees
 
